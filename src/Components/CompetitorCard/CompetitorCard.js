6aa9d0bef0b7ad51da4592761cd9cd63d32ba38b
Hannah Cryer <hannahcryer@hotmail.com>
2020-06-15 16:18:46 +0100

    Started Competitor Card Styling

--- src/Components/CompetitorCard/CompetitorCard.js
+++ src/Components/CompetitorCard/CompetitorCard.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import axios from '../../axios';
 
+import logo from '../../imgs/Logo.png';
 
 class CompetitorCard extends Component {
     constructor(props) {
@@ -13,7 +17,7 @@ class CompetitorCard extends Component {
 
     componentDidMount() {
         let { id } = this.props;
-        axios.get(`/games/api/competitors/${competitor_id}`).then(({ data }) => {
+        axios.get(`/games/api/competitors/${id}`).then(({ data }) => {
             this.setState({
                 competitor: data.data,
             });
@@ -26,13 +30,24 @@ class CompetitorCard extends Component {
 
 
         return (
-            <div className="d-flex flex-column align-items-center p-4">
-                <h2>{competitor.name}</h2>
-                <p>{competitor.bio}</p>
-                <p>{competitor.dob}</p>
-            </div>
+            <Card style={{ width: '18rem', margin: '2rem' }}>
+                <Card.Img variant="top" src={logo} />
+                
+                <Card.Body>
+                    <Card.Title>Competitor Name </Card.Title>
+                    <Card.Text>
+                        This is the bio of the competitor
+                    </Card.Text>
+                    <Button variant="info">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            // <div className="d-flex flex-column align-items-center p-4">
+            //     <h2>{competitor.name}</h2>
+            //     <p>{competitor.bio}</p>
+            //     <p>{competitor.dob}</p>
+            // </div>
         )
     }
-} 
+}
 
 export default CompetitorCard;
