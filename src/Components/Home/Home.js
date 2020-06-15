fe6f2b26050d0328840ed9639b839669dafae1cf
Jack Milton <originalmiltonz@gmail.com>
2020-06-15 12:32:27 +0100

    Started on homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+
 import Nav from 'react-bootstrap/Nav';
 import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Navbar from 'react-bootstrap/Navbar';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 
 import logo from '../../imgs/Logo.png';
 
@@ -10,20 +16,27 @@ class Home extends Component {
     render() {
         return (
             <>
-                <Nav>
-                    <Nav.Item>
-                        <Nav.Link href="home">Home</Nav.Link>
-                    </Nav.Item>
-                </Nav>
                 <div>
-                    <Image src={logo} fluid />
-                    <h1>Lockdown-Olympics</h1>
+                    <div className="landingBackground">
+                        <Row className="justify-content-md-center text-center">
+                            <Col xs={12} sm={10} md={10}>
+                                <Image src={logo} style={{ width: 800, height: 400, marginTop: 50 }} />
+                                <h1 className="header">Lockdown-Olympics</h1>
+                            </Col>
+                        </Row>
+                    </div>
+
+                    <div className="justify-content-md-center text-center">
+                        <Jumbotron style={{ height: 50, marginBottom: 0, marginTop: 100, backgroundColor: "#FCB131", }}>
+                            <a href="/"><h2><i>Categories</i></h2></a>
+                        </Jumbotron>
+                        <Jumbotron style={{ height: 50, marginBottom: 0, backgroundColor: "#f79800" }}>
+                            <a href="/"><h2><i>Competitors</i></h2></a>
+                        </Jumbotron>
+                    </div>
                 </div>
 
-                <div>
-                    <h2>Categories</h2>
-                    <h2>Competitor</h2>
-                </div>
+
 
 
             </>
