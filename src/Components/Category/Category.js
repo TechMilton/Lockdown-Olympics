5305d6e281ea7f4ae9016a8a9971fe8a002d5a99
Jack Milton <originalmiltonz@gmail.com>
2020-06-16 16:11:11 +0100

    Events page styled without background

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -12,53 +12,62 @@ class Category extends Component {
                 <div className="catergoriesCards">
 
                     {/* Home Events */}
-                    <Card className="homeEvents" style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="https://img.icons8.com/pastel-glyph/64/000000/home.png" />
-                        <Card.Body>
-                            <Card.Title style={{
-                                fontSize: "2rem", fontWeight: "500",
-                            }}>Home Events</Card.Title>
-                            < Card.Text >
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut malesuada ut quam eu malesuada.
-                    </Card.Text>
-                            <Button variant="primary"></Button>
-                        </Card.Body>
-                    </Card>
+                    <a className="homeLink" href="/">
+                        <Card className="homeEvents" style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="https://img.icons8.com/pastel-glyph/64/000000/home.png" />
+                            <Card.Body>
+                                <Card.Title style={{
+                                    fontSize: "2rem", fontWeight: "500",
+                                }}>Home Events</Card.Title>
+                                <Card.Text>
+                                    Go Hard <span className="line">or Go Home</span> and Stay Home!
+                            </Card.Text>
+                                <Button variant="primary"></Button>
+                            </Card.Body>
+                        </Card>
+                    </a>
+
                     {/* Garden Events */}
-                    <Card className="gardenEvents" style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="https://img.icons8.com/ios-filled/50/000000/spade.png" />
-                        <Card.Body>
-                            <Card.Title style={{ fontSize: "2rem", fontWeight: "500", }}>Garden Events</Card.Title>
-                            <Card.Text>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut malesuada ut quam eu malesuada.
+                    <a className="gardenLink" href="/">
+                        <Card className="gardenEvents" style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="https://img.icons8.com/ios-filled/50/000000/spade.png" />
+                            <Card.Body>
+                                <Card.Title style={{ fontSize: "2rem", fontWeight: "500", }}>Garden Events</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum dolor sit amet
                     </Card.Text>
-                            <Button variant="primary"></Button>
-                        </Card.Body>
-                    </Card>
+                                <Button variant="primary"></Button>
+                            </Card.Body>
+                        </Card>
+                    </a>
 
                     {/* Mental Events */}
-                    <Card className="mentalEvents" style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="https://img.icons8.com/ios-filled/50/000000/brain.png" />
-                        <Card.Body>
-                            <Card.Title style={{ fontSize: "2rem", fontWeight: "500", }}>Mental Events</Card.Title>
-                            <Card.Text>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut malesuada ut quam eu malesuada.
-                    </Card.Text>
-                            <Button variant="primary"></Button>
-                        </Card.Body>
-                    </Card>
+                    <a className="mentalLink" href="/">
+                        <Card className="mentalEvents" style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="https://img.icons8.com/ios-filled/50/000000/brain.png" />
+                            <Card.Body>
+                                <Card.Title style={{ fontSize: "2rem", fontWeight: "500", }}>Mental Events</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum dolor sit amet
+                            </Card.Text>
+                                <Button variant="primary"></Button>
+                            </Card.Body>
+                        </Card>
+                    </a>
 
-
-                    <Card className="foodEvents" style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="" />
-                        <Card.Body>
-                            <Card.Title style={{ fontSize: "2rem", fontWeight: "500", }}>Mental Events</Card.Title>
-                            <Card.Text>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut malesuada ut quam eu malesuada.
-                    </Card.Text>
-                            <Button variant="primary"></Button>
-                        </Card.Body>
-                    </Card>
+                    {/* Food Events */}
+                    <a className="foodLink" href="/">
+                        <Card className="foodEvents" style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="https://img.icons8.com/material/24/000000/hamburger--v1.png" />
+                            <Card.Body>
+                                <Card.Title style={{ fontSize: "2rem", fontWeight: "500", }}>Food Events</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum dolor sit amet
+                            </Card.Text>
+                                <Button variant="primary"></Button>
+                            </Card.Body>
+                        </Card>
+                    </a>
                 </div>
             </>
 
