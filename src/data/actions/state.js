30a6e077ebda9ca4dcc6cd174a0de22878b37fcd
mdm106 <mads.mcmahon@googlemail.com>
2020-06-16 16:17:24 +0100

    State and api functions for retrieving competitor list

---
+++ src/data/actions/state.js
@@ -0,0 +1,13 @@
+export const saveCompetitors  = ( data ) => {
+    return {
+        type: "SAVE_COMPETITORS",
+        competitors: data,
+    };
+}
+
+
+
+
+
+
+// export const addCompetitorDetails ( )
