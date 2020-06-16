dbcf2ab139c399145b838bc5647148e70965ef12
mdm106 <mads.mcmahon@googlemail.com>
2020-06-16 16:07:27 +0100

    Axios page setup

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
-
-export default axios;
+export default axios.create({
+    baseURL: "https://games/api",
+    headers: {
+        // make sure we get JSON back
+        Accept: "application/json",
+    },
+});
 
