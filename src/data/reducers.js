a91af0d40425149798b7d3d96da7246db0480262
mdm106 <mads.mcmahon@googlemail.com>
2020-06-16 16:33:34 +0100

    SaveCompetitors functions added to reducer

--- src/data/reducers.js
+++ src/data/reducers.js
@@ -1,6 +1,17 @@
-import { createStore } from "redux";
+import initial from './initial';
+
+const saveCompetitorsReducer = (state, {competitors}) => {
+    return {
+        ...state,
+        competitors,
+    }
+}
 
 const reducer = (state, action) => {
+    switch (action.type) {
+        case "SAVE_COMPETITORS": return saveCompetitorsReducer(state, action);
+        default: return state;
+    }
     
 }
 
