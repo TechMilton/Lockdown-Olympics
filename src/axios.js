359890e37e14f4199d4a0ab5b08ed7d23e2ac910
mdm106 <mads.mcmahon@googlemail.com>
2020-06-16 23:15:10 +0100

    Axios baseURL updated

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://games/api",
+    baseURL: "http://games/api",
     headers: {
         // make sure we get JSON back
         Accept: "application/json",
