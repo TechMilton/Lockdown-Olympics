d3aa853487ccc6de05c1316ec6222a88e7e91165
Jack Milton <originalmiltonz@gmail.com>
2020-06-16 11:09:52 +0100

    Started on NavBar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,17 +5,30 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 import logo from '../../imgs/Logo.png';
-import grassHome from '../../imgs/grassHome.png';
-
+import grass from '../../imgs/grass.png';
 
 class Home extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    handleClickEvent(e) {
+
+    }
+
+    handleClickCompetitors(e) {
+
+    }
 
     render() {
         return (
             <>
-                <div>
-                    <div >
+                <div class="backgroundLanding">
+                    <div>
                         <Row className="homePageHeader">
                             <Col xs={12} sm={10} md={10} className="colClass">
                                 <Image src={logo} style={{ width: 800, height: 400, }} />
@@ -24,14 +37,13 @@ class Home extends Component {
                         </Row>
                     </div>
                     <nav className="homePageCards">
-                        <Image src={grassHome} className="grass" />
+                        <Image src={grass} className="grass" />
                         <div className="Links link1">
-                            <a className="cardLinks" href="/"><h3>CATEGORIES</h3></a>
+                            <a className="cardLinks" href="/" onClick={this.handleClickEvent} ><h3>EVENT CATEGORIES</h3></a>
                         </div>
                         <div className="Links link2">
-                            <a className="cardLinks" href="/"><h3>COMPETITORS</h3></a>
+                            <a className="cardLinks" href="/" onClick={this.handleClickCompetitors} ><h3>COMPETITORS</h3></a>
                         </div>
-
                     </nav >
                 </div>
 
