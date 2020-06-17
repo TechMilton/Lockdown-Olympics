967c49a27ed62b8f4c92f2c9da57f3b206c3dbc0
mdm106 <mads.mcmahon@googlemail.com>
2020-06-17 13:16:30 +0100

    CompetitorList names made into links which go to competitor card when clicked

--- src/Components/CompetitorList/CompetitorList.js
+++ src/Components/CompetitorList/CompetitorList.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import sports from '../../../src/imgs/sports.jpg';
 import Alert from 'react-bootstrap/Alert'
@@ -23,7 +24,9 @@ const CompetitorList = (
                  <div className="competitorList"> {
                     competitors.map((competitor, index) =>
                         <div key={index} className="competitorListItem">
-                            <a href="/competitor"><h2 className="titleFont">{competitor.name}</h2></a>
+                             <Link to={`/competitors/${competitor.id}`}>
+                                { competitor.name }
+                            </Link>
                         </div>
                     )}
                 </div>
