1a93aff51130bef6de765495dda48a8ebee6ccd9
mdm106 <mads.mcmahon@googlemail.com>
2020-06-17 16:10:36 +0100

    Commented out function removed from api.js

--- src/data/actions/api.js
+++ src/data/actions/api.js
@@ -10,15 +10,6 @@ export const getCompetitors = () => {
     }
 }
 
-// export const getCompetitor = (id) => {
-//     return (dispatch) => {
-//         return axios.get(`/competitors/${id}`).then(({ data }) => {
-//             dispatch(addCompetitorDetails(data.data));
-//             })
-//         }
-//     }
-
-///from our discussion with Jack
 export const getCompetitor = (id) => (dispatch, getState) => {
     const isComplete = getState().competitors.find(competitor => competitor.id === id && competitor.detailsAdded);
 
