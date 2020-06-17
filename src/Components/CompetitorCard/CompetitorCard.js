c1a2b2bee723473736b2fa85008e8045dcc48f2b
Hannah Cryer <hannahcryer@hotmail.com>
2020-06-17 14:41:41 +0100

    deleted a space

--- src/Components/CompetitorCard/CompetitorCard.js
+++ src/Components/CompetitorCard/CompetitorCard.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
@@ -19,10 +20,11 @@ const CompetitorCard = (
         handleCompetitorLoad()
     }, [])
 
+    
     return (<div className="backgroundImage">
         <PageHeading>Competitor Name</PageHeading>
         <div>
-            <Loading loaded={competitor !== undefined}>
+            {competitor === undefined ? <Loading loaded={false} /> :
                 <Card className="competitorCard">
                     <Card.Img
                         variant="top"
@@ -32,18 +34,25 @@ const CompetitorCard = (
                         }} />
 
                     <Card.Body className="m-auto">
-                        <h1 className='p-2 titleFont'>Competitor Name</h1>
-                        <h4 className='p-3 italicFont'>This is the bio of said competitor</h4>
+                        <h1 className='p-2 titleFont'>{competitor.name}</h1>
+                        <p className='p-3 italicFont'>{competitor.bio}</p>
                         <h6 className="titleFont">Date Of Birth:</h6>
-                        <p className="italicFont">04/05/1995</p>
+                        <p className="italicFont">{competitor.dob}</p>
+                        <h6 className="titleFont">Competing in:</h6>
+                        <p className="italicFont">{competitor.sport}</p>
                     </Card.Body>
+                    <div className="competitorListItem">
+                        <Link to={`/competitors`}>
+                            <h2 className="titleFont">Back to competitors</h2>
+                        </Link>
+                    </div>
                 </Card>
-            </Loading>
+            }
         </div>
     </div>
 
     );
-                    
+
 
 }
 
