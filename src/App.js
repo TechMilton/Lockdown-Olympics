e59dc1f7f30941e706ef85a091fd1b941efebc7c
mdm106 <mads.mcmahon@googlemail.com>
2020-06-17 15:58:37 +0100

    SportsCardDuo removed from app import

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Home from './Components/Home/Home';
 import FourOhFour from './Components/FourOhFour';
 import CompetitorCard from './Components/CompetitorCard';
 import CustomNavBar from './Components/CustomNavBar';
-import SportsCardDuo from './Components/SportsCardDuo';
 import SportsCard from './Components/SportsCard';
 import Category from './Components/Category/Category';
 import CompetitorList from './Components/CompetitorList';
@@ -34,12 +33,12 @@ const App = () => (
           <CompetitorCard competitorId={match.params.id} />
         )} />
 
-        <Route path="/categories">
+        <Route exact path="/categories">
           <CustomNavBar />
           <Category />
         </Route>
 
-        <Route exact path="/categories/:id/sports" render={({ match }) => (
+        <Route path="/categories/:id/sports" render={({ match }) => (
           <SportsCard categoryId={match.params.id} />
         )} />
 
