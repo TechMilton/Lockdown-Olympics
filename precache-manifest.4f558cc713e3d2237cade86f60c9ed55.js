1588dcf479584fdc0ea3d024b8e97932e2b0c8e0
Jack Milton <originalmiltonz@gmail.com>
2020-06-19 13:03:53 +0100

    Updates

---
+++ precache-manifest.4f558cc713e3d2237cade86f60c9ed55.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8234b3692d27400ef569a71b37bc25",
+    "url": "/Lockdown-Olympics/index.html"
+  },
+  {
+    "revision": "648783e4165ea5538013",
+    "url": "/Lockdown-Olympics/static/css/main.b947c275.chunk.css"
+  },
+  {
+    "revision": "8c69b079af5864f21586",
+    "url": "/Lockdown-Olympics/static/js/2.8a7b497d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Lockdown-Olympics/static/js/2.8a7b497d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "648783e4165ea5538013",
+    "url": "/Lockdown-Olympics/static/js/main.078426e8.chunk.js"
+  },
+  {
+    "revision": "e7c2f419ad9c81d61d00",
+    "url": "/Lockdown-Olympics/static/js/runtime-main.03c00319.js"
+  },
+  {
+    "revision": "592372862007069d84bb8181a09a874d",
+    "url": "/Lockdown-Olympics/static/media/1.59237286.png"
+  },
+  {
+    "revision": "5cf590eef9933c4ec21d04b672316521",
+    "url": "/Lockdown-Olympics/static/media/2.5cf590ee.png"
+  },
+  {
+    "revision": "f5b3412dff6f632228491e49bbf3f5f0",
+    "url": "/Lockdown-Olympics/static/media/3.f5b3412d.png"
+  },
+  {
+    "revision": "4b9582110c4fe66c2de70a4bdfb95556",
+    "url": "/Lockdown-Olympics/static/media/4.4b958211.png"
+  },
+  {
+    "revision": "828206c9345b106a51b4a32ccab555cf",
+    "url": "/Lockdown-Olympics/static/media/5.828206c9.png"
+  },
+  {
+    "revision": "f4fe032d9748f61fb27f6d031ea782f7",
+    "url": "/Lockdown-Olympics/static/media/6.f4fe032d.png"
+  },
+  {
+    "revision": "dc861a1fdbc49025d2cbd3c267450ff2",
+    "url": "/Lockdown-Olympics/static/media/7.dc861a1f.png"
+  },
+  {
+    "revision": "c772e9818597b09da5dbe382ae8f05af",
+    "url": "/Lockdown-Olympics/static/media/8.c772e981.png"
+  },
+  {
+    "revision": "1587db51f5edcb6c37b3c7b2b01423b7",
+    "url": "/Lockdown-Olympics/static/media/LONDO___.1587db51.svg"
+  },
+  {
+    "revision": "18b1ec8e2b57c115a670b9290cacf981",
+    "url": "/Lockdown-Olympics/static/media/LONDO___.18b1ec8e.woff"
+  },
+  {
+    "revision": "8a1ea2e0b898ccb49141ec704c948736",
+    "url": "/Lockdown-Olympics/static/media/LONDO___.8a1ea2e0.eot"
+  },
+  {
+    "revision": "92b1027247f25e964c7c80b7cd021f5d",
+    "url": "/Lockdown-Olympics/static/media/LONDO___.92b10272.ttf"
+  },
+  {
+    "revision": "205c950972ae8327165d252fcd3df95c",
+    "url": "/Lockdown-Olympics/static/media/Logo.205c9509.png"
+  },
+  {
+    "revision": "f9318583bd8987741384ae1b22e2749b",
+    "url": "/Lockdown-Olympics/static/media/baseball.f9318583.png"
+  },
+  {
+    "revision": "68f6ecfe7f0cdaab50aa2b38dd3faaaa",
+    "url": "/Lockdown-Olympics/static/media/bgsport.68f6ecfe.jpg"
+  },
+  {
+    "revision": "aef2e540f81037a479e01ac5fbd3fe30",
+    "url": "/Lockdown-Olympics/static/media/sports.aef2e540.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TechMilton/Lockdown-Olympics/precache-manifest.ba6f488a6a4b25dc2908e127958fddd3.js"
+  "/Lockdown-Olympics/precache-manifest.4f558cc713e3d2237cade86f60c9ed55.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TechMilton/Lockdown-Olympics/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Lockdown-Olympics/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
