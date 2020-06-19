4852bcce2c3a8a888633ecd5e86e07d47535eb24
Jack Milton <originalmiltonz@gmail.com>
2020-06-19 13:02:11 +0100

    Updates

---
+++ precache-manifest.ba6f488a6a4b25dc2908e127958fddd3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f58336c706aed091f12709602978cf",
+    "url": "/TechMilton/Lockdown-Olympics/index.html"
+  },
+  {
+    "revision": "86d38bd28188edefc1eb",
+    "url": "/TechMilton/Lockdown-Olympics/static/css/main.3d52dda3.chunk.css"
+  },
+  {
+    "revision": "8c69b079af5864f21586",
+    "url": "/TechMilton/Lockdown-Olympics/static/js/2.8a7b497d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TechMilton/Lockdown-Olympics/static/js/2.8a7b497d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d38bd28188edefc1eb",
+    "url": "/TechMilton/Lockdown-Olympics/static/js/main.d5c464f8.chunk.js"
+  },
+  {
+    "revision": "302e87c53d15c4933932",
+    "url": "/TechMilton/Lockdown-Olympics/static/js/runtime-main.c8f8db8a.js"
+  },
+  {
+    "revision": "592372862007069d84bb8181a09a874d",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/1.59237286.png"
+  },
+  {
+    "revision": "5cf590eef9933c4ec21d04b672316521",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/2.5cf590ee.png"
+  },
+  {
+    "revision": "f5b3412dff6f632228491e49bbf3f5f0",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/3.f5b3412d.png"
+  },
+  {
+    "revision": "4b9582110c4fe66c2de70a4bdfb95556",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/4.4b958211.png"
+  },
+  {
+    "revision": "828206c9345b106a51b4a32ccab555cf",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/5.828206c9.png"
+  },
+  {
+    "revision": "f4fe032d9748f61fb27f6d031ea782f7",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/6.f4fe032d.png"
+  },
+  {
+    "revision": "dc861a1fdbc49025d2cbd3c267450ff2",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/7.dc861a1f.png"
+  },
+  {
+    "revision": "c772e9818597b09da5dbe382ae8f05af",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/8.c772e981.png"
+  },
+  {
+    "revision": "1587db51f5edcb6c37b3c7b2b01423b7",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/LONDO___.1587db51.svg"
+  },
+  {
+    "revision": "18b1ec8e2b57c115a670b9290cacf981",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/LONDO___.18b1ec8e.woff"
+  },
+  {
+    "revision": "8a1ea2e0b898ccb49141ec704c948736",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/LONDO___.8a1ea2e0.eot"
+  },
+  {
+    "revision": "92b1027247f25e964c7c80b7cd021f5d",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/LONDO___.92b10272.ttf"
+  },
+  {
+    "revision": "205c950972ae8327165d252fcd3df95c",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/Logo.205c9509.png"
+  },
+  {
+    "revision": "f9318583bd8987741384ae1b22e2749b",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/baseball.f9318583.png"
+  },
+  {
+    "revision": "68f6ecfe7f0cdaab50aa2b38dd3faaaa",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/bgsport.68f6ecfe.jpg"
+  },
+  {
+    "revision": "aef2e540f81037a479e01ac5fbd3fe30",
+    "url": "/TechMilton/Lockdown-Olympics/static/media/sports.aef2e540.jpg"
+  }
+]);
