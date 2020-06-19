c0b26d9389e0a33d7f5331364803c150afbd9ede
Adam Gardner <adam.gardner4991@gmail.com>
2020-06-19 10:27:52 +0100

    sports page button and header styled

--- src/Components/SportsCard/SportsCard.js
+++ src/Components/SportsCard/SportsCard.js
@@ -30,7 +30,7 @@ const SportsCard = (
     return (
         <div className="backgroundImage">
 
-            <PageHeading>{titlePicker(categoryId) + " Events"}</PageHeading>
+            <PageHeading><h1 className="heading competitorCardHeading ">{titlePicker(categoryId) + " Events"}</h1></PageHeading>
             <Loading loaded={sports.length}>
                 <div className="sportCardGroup">
                     {sports.map((sport, index) =>
@@ -44,21 +44,21 @@ const SportsCard = (
                                 }} />
 
                             <Card.Body className="m-auto">
-                                <h1 className='p-2 titleFont'>{sport.name}</h1>
+                                <h1 className='p-2 cardTitleFont'>{sport.name}</h1>
                                 <p className='p-3 italicFont'>{sport.description}</p>
-                                <h5 className='titleFont'>{`Start Time: ${sport.start_time}`}</h5>
-                                <h5 className='titleFont'>{`Location: ${sport.arena} Arena`}</h5>
+                                <h5 className='cardTitleFont'>{`Start Time: ${sport.start_time}`}</h5>
+                                <h5 className='cardTitleFont'>{`Location: ${sport.arena} Arena`}</h5>
                             </Card.Body>
                         </Card>
                     )}
                 </div>
                 <Link
                     to={`/categories`}
-                    className="backBtn"
+                    className="heading backBtn"
                     style={{ textDecoration: "none" }}
                 >
-                    <i class="fas fa-arrow-left" style={{ marginRight: 12 }}></i>Back to categories
-                </Link>
+                    <i class="fas fa-arrow-left"></i>Back to event categories
+                    </Link>
             </Loading>
         </div>
     )
